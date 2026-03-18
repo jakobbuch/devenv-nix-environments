817c6ef38b64aaed5726ba994c83f433b0675d64
jakobbuch <81036574+jakobbuch@users.noreply.github.com>
2026-03-18 14:32:08 +0100

    refactor: restructure markdownlint configuration in git hooks

--- devenv/markdown-hooks-dev.nix
+++ devenv/markdown-hooks-dev.nix
@@ -1,6 +1,12 @@
 _: {
   # https://devenv.sh/git-hooks/
   git-hooks.hooks = {
-    markdownlint.enable = true;
+    markdownlint = {
+      enable = true;
+      settings.configuration = {
+        default = true;
+        MD013 = false; # Line length
+      };
+    };
   };
 }
