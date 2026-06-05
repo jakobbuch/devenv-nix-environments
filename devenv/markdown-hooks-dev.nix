227449a560c41f2ed410ddf7165cffcc2dcb9c80
jakobbuch <81036574+jakobbuch@users.noreply.github.com>
2026-06-05 15:06:42 +0200

    fix: disable additional markdownlint rules in markdown-hooks configuration

--- devenv/markdown-hooks-dev.nix
+++ devenv/markdown-hooks-dev.nix
@@ -6,8 +6,15 @@ _: {
       args = [ "--fix" ];
       settings.configuration = {
         default = true;
+        MD003 = false; # Header style rule disable
+        MD005 = false; # Inconsistent indentation rule disable
+        MD007 = false; # Unordered list indentation rule disable
         MD013 = false; # Line length rule disable
+        MD029 = false; # Ordered list item prefix rule disable
         MD036 = false; # Emphasis used instead of a header
+        MD041 = false; # First line in file should be a top level header
+        MD050 = false; # Disable to allow both ** and __ for strong emphasis
+        MD060 = false; # Consider first line of file as a header
       };
     };
   };
